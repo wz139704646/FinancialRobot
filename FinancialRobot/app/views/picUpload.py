99e59b01caa4c7d9c2069896e594ab1a10a9a207
Yunlin <mao.looper@qq.com>
2019-07-07 00:17:30 +0800

    0707-myl-back add photo

--- FinancialRobot/app/views/picUpload.py
+++ FinancialRobot/app/views/picUpload.py
@@ -4,6 +4,8 @@ from werkzeug.utils import secure_filename
 from flask import Blueprint, render_template, request, make_response, send_from_directory, abort
 import os
 import json
+
+from app.dao.GoodsDao import GoodsDao
 from app.utils.pic_str import *
 from app.utils.res_json import *
 
@@ -30,6 +32,7 @@ def api_upload():
     if not os.path.exists(file_dir):
         os.makedirs(file_dir)
     f, = request.files.values()
+    id = request.json.get("id")
     size = len(f.read())
     print(size)
     f.seek(0)
@@ -41,6 +44,8 @@ def api_upload():
             new_filename = Pic_str().create_uuid() + '.' + ext
             f.save(os.path.join(file_dir, new_filename))
             f.close()
+            goods_dao = GoodsDao()
+            goods_dao.update_photo(id, new_filename)
             return json.dumps(return_success({"new_filename": new_filename}), ensure_ascii=False)
         else:
             return json.dumps(return_unsuccess("文件大小超出1MB"), ensure_ascii=False)
