226c4e257bacc80e3777be6126283b1f256e9f1e
WHU-Yh-jx <1092949763@qq.com>
2019-07-07 10:40:59 +0800

    7-7-hjx-用户注册修改

--- FinancialRobot/manage.py
+++ FinancialRobot/manage.py
@@ -3,5 +3,6 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    #app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
     # app.run()
