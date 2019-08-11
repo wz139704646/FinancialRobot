7b188e234e8e232edc779142308d0cc0b161dda6
zjj <632291793@qq.com>
2019-08-12 00:37:31 +0800

    0812-zjj-finance add sql file

---
+++ FinancialRobot/app/static/sql/finance_tbls.sql
@@ -0,0 +1,50 @@
+CREATE TABLE IF NOT EXISTS accounting_subjects(
+	subject_code VARCHAR(15),
+	name VARCHAR(20) NOT NULL,
+	superior_subject_code VARCHAR(15),
+	type VARCHAR(10) NOT NULL,
+	type_detail VARCHAR(10),
+	PRIMARY KEY (subject_code),
+	FOREIGN KEY (superior_subject_code) REFERENCES accounting_subjects(subject_code) ON DELETE RESTRICT ON UPDATE RESTRICT
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS general_voucher (
+    date DATETIME NOT NULL,
+	record_date DATETIME NOT NULL DEFAULT NOW(),
+	voucher_no VARCHAR(10),
+	attachments_number TINYINT NOT NULL,
+	checked TINYINT NOT NULL DEFAULT 0 CHECK ( checked in (0, 1) ),
+	PRIMARY KEY (voucher_no)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS accounting_subjects_balance(
+	time VARCHAR(7) NOT NULL,
+	subject_code VARCHAR(15) NOT NULL,
+	opening_balance DECIMAL(19, 4) NOT NULL,
+	credit DECIMAL(19, 4) NOT NULL,
+	debit DECIMAL(19, 4) NOT NULL,
+	PRIMARY KEY (time, subject_code),
+	FOREIGN KEY (subject_code) REFERENCES accounting_subjects(subject_code) ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS voucher_entry (
+	voucher_no VARCHAR(10) NOT NULL,
+	abstract VARCHAR(30) NOT NULL,
+	subject_code VARCHAR(15) NOT NULL,
+	credit_debit VARCHAR(1) check ( credit_debit in ("借", "贷") ),
+	total DECIMAL(19, 4) NOT NULL,
+	FOREIGN KEY (voucher_no) REFERENCES general_voucher(voucher_no) ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (subject_code) REFERENCES accounting_subjects(subject_code) ON DELETE RESTRICT ON UPDATE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE IF NOT EXISTS voucher_attachment (
+	voucher_no VARCHAR(10) NOT NULL,
+	attachment_url VARCHAR(100) NOT NULL,
+	for_voucher TINYINT NOT NULL DEFAULT 0,
+	FOREIGN KEY (voucher_no) REFERENCES general_voucher(voucher_no) ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
