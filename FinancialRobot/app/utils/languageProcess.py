69b0bcb0cc103963f8d0124381c9d940f202a2fa
Yunlin <mao.looper@qq.com>
2019-11-23 20:47:26 +0800

    11-23-myl-change dir

--- FinancialRobot/app/utils/languageProcess.py
+++ FinancialRobot/app/utils/languageProcess.py
@@ -23,8 +23,8 @@ from .recommend import recommend
 #LOCATE='http://127.0.0.1:5000' #电脑端
 lanprocess = Blueprint("lanprocess", __name__)
 
-#UPLOAD_FOLDER = 'app\\utils\\dict.txt'#服务器端
-UPLOAD_FOLDER = 'dict.txt'#电脑端
+UPLOAD_FOLDER = 'app/utils/dict.txt'#服务器端
+#UPLOAD_FOLDER = 'dict.txt'#电脑端
 basedir = os.getcwd()
 #basedir = os.path.abspath(os.path.dirname(__file__))
 file_dir = os.path.join(basedir, UPLOAD_FOLDER)
